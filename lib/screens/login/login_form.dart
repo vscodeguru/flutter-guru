854fcd1877f279aa1df68ead04cca0d105eea439
ralph noel <brunoralphjr@gmail.com>
2019-03-27 20:41:10 +0530

    Half of the UI finished

--- lib/screens/login/login_form.dart
+++ lib/screens/login/login_form.dart
@@ -44,30 +44,91 @@ class _LoginFormState extends State<LoginForm> {
           });
         }
 
-        return Form(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'username'),
-                controller: _usernameController,
+        return new Scaffold(
+          body: new Stack(
+              children: <Widget>[
+             
+              Positioned(
+                width: 400.0,
+                top: MediaQuery.of(context).size.height / 5,
+                child: Column(
+                  children: <Widget>[
+                    // CircleAvatar(
+                    //   backgroundImage: AssetImage('assets/300_3.jpg'),
+                    //   radius: 60.0,
+                    // )
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      width: 150.0,
+                      height: 150.0,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          image: DecorationImage(
+                              image: AssetImage('assets/300_3.jpg'),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                          boxShadow: [
+                            BoxShadow(blurRadius: 7.0, color: Colors.indigo)
+                          ]),
+                    ),
+                  ],
+                ),
               ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'password'),
-                controller: _passwordController,
-                obscureText: true,
-              ),
-              RaisedButton(
-                onPressed:
-                    state is! LoginLoading ? _onLoginButtonPressed : null,
-                child: Text('Login'),
+                ClipPath(
+                 child: Container(
+                   
+                   color: Colors.indigo.withOpacity(0.8),
+                 ),
+                 clipper: GetClipper()
+               ),
+              SingleChildScrollView(
+                
+                padding: EdgeInsets.fromLTRB(15.0, 385.0, 0.0, 0.0),
+                child: TextField(
+                   decoration: InputDecoration(
+                      labelText: 'Enter your number',
+                      labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                ),
+                
               ),
               Container(
-                child:
-                    state is LoginLoading ? CircularProgressIndicator() : null,
-              ),
+                height: 40.0,
+                child: Material(
+
+                ),
+              )
             ],
+
           ),
         );
+
+        // return Form(
+        //   child: Column(
+        //     children: [
+        //       TextFormField(
+        //         decoration: InputDecoration(labelText: 'username'),
+        //         controller: _usernameController,
+        //       ),
+        //       TextFormField(
+        //         decoration: InputDecoration(labelText: 'password'),
+        //         controller: _passwordController,
+        //         obscureText: true,
+        //       ),
+        //       RaisedButton(
+        //         onPressed:
+        //             state is! LoginLoading ? _onLoginButtonPressed : null,
+        //         child: Text('Login'),
+        //       ),
+        //       Container(
+        //         child:
+        //             state is LoginLoading ? CircularProgressIndicator() : null,
+        //       ),
+        //     ],
+        //   ),
+        // );
       },
     );
   }
@@ -83,5 +144,23 @@ class _LoginFormState extends State<LoginForm> {
       username: _usernameController.text,
       password: _passwordController.text,
     ));
+  }
+}
+
+class GetClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+     path.lineTo(500, 500);
+     path.lineTo(-500, 500);
+     path.lineTo(-500, 0);
+     
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -35,9 +35,9 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('Login'),
+      // ),
       body: LoginForm(
         authenticationBloc: _authenticationBloc,
         loginBloc: _loginBloc,
