7ac76c4ff5585b2eada6164dceb1f5b6703bcb74
vscodeguru <vscodeguru@gmail.com>
2019-03-27 11:52:07 +0530

    source inital commit

---
+++ lib/widgets/loading_indicator/loading_indicator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Center(
+        child: SpinKitFadingCircle(itemBuilder: (_, int index) {
+          return DecoratedBox(
+            decoration: BoxDecoration(
+              color: index.isEven ? Colors.red : Colors.green,
+            ),
+          );
+        }),
+      );
+}
