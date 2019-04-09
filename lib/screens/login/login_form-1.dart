096b27f91be40de9f0740ebdb1dfd8602f645324
ralphnoel13 <brunoralphjr@gmail.com>
2019-04-09 11:39:37 +0530

    pubspec.lock changes

--- lib/screens/login/login_form-1.dart
+++ lib/screens/login/login_form-1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:flutter/animation.dart';
 import 'package:flutter_guru/screens/login/index.dart';
 import 'package:flutter_guru/utils/authentication/index.dart';
 
@@ -20,7 +20,8 @@ class LoginForm1 extends StatefulWidget {
   State<LoginForm1> createState() => _LoginForm1State();
 }
 
-class _LoginForm1State extends State<LoginForm1> {
+class _LoginForm1State extends State<LoginForm1>
+    with SingleTickerProviderStateMixin {
   TextEditingController _textEditingController = TextEditingController();
   String _mobile;
   bool _autoValidate = false;
@@ -29,35 +30,6 @@ class _LoginForm1State extends State<LoginForm1> {
   final _mobileController = TextEditingController();
   final _otpController = TextEditingController();
   LoginBloc get _loginBloc => widget.loginBloc;
-  // final mobilenumber = TextField(
-  //    controller: _usernameController,
-
-  //   keyboardType: TextInputType.phone,
-  //   autofocus: false,
-  //   decoration: InputDecoration(
-  //     labelText: 'Mobile',
-  //     labelStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
-  //     focusedBorder:
-  //         UnderlineInputBorder(borderSide: BorderSide(color: Colors.green)),
-  //     hintText: 'Enter your Mobile Number',
-  //     contentPadding: EdgeInsets.fromLTRB(0.0, 10.0, 20.0, 10.0),
-  //   ),
-  // );
-  // final loginbutton = Padding(
-  //   padding: EdgeInsets.only(left: 100.0),
-  //   child: RaisedButton(
-  //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
-  //     onPressed: (
-  //     ) {},
-  //     color: Colors.black87,
-  //     child: Text(
-  //       'Next',
-  //       style: TextStyle(
-  //         color: Colors.white,
-  //       ),
-  //     ),
-  //   ),
-  // );
 
   @override
   Widget build(BuildContext context) {
@@ -113,7 +85,6 @@ class _LoginForm1State extends State<LoginForm1> {
                   ],
                 ),
               ),
-
               Container(
                 child: Stack(
                   children: <Widget>[
@@ -132,8 +103,8 @@ class _LoginForm1State extends State<LoginForm1> {
               ),
               SingleChildScrollView(
                 //  shrinkWrap: true,
-                  padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 420),
-                    child:Column(
+                padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 420),
+                child: Column(
                   children: <Widget>[
                     _isTextFieldVisible
                         ? TextFormField(
@@ -159,6 +130,7 @@ class _LoginForm1State extends State<LoginForm1> {
                               WhitelistingTextInputFormatter.digitsOnly,
                             ],
                             decoration: InputDecoration(
+                              suffixIcon: Icon(Icons.phone_android),
                               labelText: 'Mobile',
                               labelStyle: TextStyle(
                                 fontWeight: FontWeight.bold,
@@ -178,6 +150,17 @@ class _LoginForm1State extends State<LoginForm1> {
                               WhitelistingTextInputFormatter.digitsOnly,
                             ],
                             decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  icon: Icon(Icons.refresh),
+                                  tooltip: 'Resend OTP',
+                                  onPressed: () {
+                                    Scaffold.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('ReSend OTP'),
+                                      ),
+                                    );
+                                  },
+                                ),
                                 labelText: 'OTP',
                                 labelStyle: TextStyle(
                                   fontWeight: FontWeight.bold,
@@ -225,74 +208,39 @@ class _LoginForm1State extends State<LoginForm1> {
                             child: Text('Login'),
                             textColor: Colors.white,
                           ),
-                    SizedBox(
-                      height: 0.0,
-                    ),
-                    //    Positioned(
-                    //     child: Container(
-
-                    //     ),
-                    //  )
-                  ])
-
-                  // Container(
-                  //   padding: EdgeInsets.fromLTRB(10.0, 135, 0.0, 0.0),
-                  //   height: 40.0,
-                  //   child: Material(
-                  //     borderRadius: BorderRadius.circular(20.0),
-                  //     shadowColor: Colors.greenAccent,
-                  //     color: Colors.green,
-                  //     elevation: 7.0,
-                  //     child: RaisedButton(
-                  //       shape:RoundedRectangleBorder(
-                  //         borderRadius: BorderRadius.circular(20.0),
-                  //       ),
-                  //       color: Colors.green,
-                  //       onPressed: (){
-
-                  //       },
-                  //       child: Center(
-                  //         child: Text(
-                  //           'Submit'
-                  //         ),
-                  //       ),
-                  //     ),
-                  ),    //   ),
+                  ],
+                ),
+              ),
             ],
-            ),
-
-              // )
-        
-            
-          );
-      }
+          ),
         );
-      
-        // return Form(
-        //   child: Column(
-        //     children: [
-        //       TextFormField(
-        //         decoration: InputDecoration(labelText: 'username'),
-        //         controller: _usernameController,
-        //       ),
-        //       TextFormField(
-        //         decoration: InputDecoration(labelText: 'password'),
-        //         controller: _passwordController,
-        //         obscureText: true,
-        //       ),
-        //       RaisedButton(
-        //         onPressed:
-        //             state is! LoginLoading ? _onLoginButtonPressed : null,
-        //         child: Text('Login'),
-        //       ),
-        //       Container(
-        //         child:
-        //             state is LoginLoading ? CircularProgressIndicator() : null,
-        //       ),
-        //     ],
-        //   ),
-        // );
-      
+      },
+    );
+
+    // return Form(
+    //   child: Column(
+    //     children: [
+    //       TextFormField(
+    //         decoration: InputDecoration(labelText: 'username'),
+    //         controller: _usernameController,
+    //       ),
+    //       TextFormField(
+    //         decoration: InputDecoration(labelText: 'password'),
+    //         controller: _passwordController,
+    //         obscureText: true,
+    //       ),
+    //       RaisedButton(
+    //         onPressed:
+    //             state is! LoginLoading ? _onLoginButtonPressed : null,
+    //         child: Text('Login'),
+    //       ),
+    //       Container(
+    //         child:
+    //             state is LoginLoading ? CircularProgressIndicator() : null,
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 
 // void _validateInputs() {
@@ -313,17 +261,6 @@ class _LoginForm1State extends State<LoginForm1> {
     _formKey..currentState.save();
     final Map<String, dynamic> product = {'Mobile': _mobile};
   }
-  // _sendToServer() {
-  //   if (_formKey.currentState.validate()) {
-  //     print('mobile $_mobile');
-
-  //     _formKey.currentState.save();
-  //   } else {
-  //     setState(() {
-  //       _validate = true;
-  //     });
-  //   }
-  // }
 
   String validateMobile(String value) {
     String patttern = r'(^[0-9]*$)';
@@ -356,11 +293,9 @@ class GetClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = new Path();
- //   path.moveTo(0, size.height * 0.3);
+
     path.lineTo(0, 350);
     path.lineTo(500, 0);
-
- //   path.lineTo(0, size.height);
     path.close();
     return path;
   }
